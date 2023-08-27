835924c738b55737bfa38fbbbda73aa19bc8d031
tenkai <tenkai@kariya.ai>
2023-08-27 15:41:26 -0700

    feat: Add `rustyline` crate for enhanced user input handling
    - Added `rustyline` crate for input handling
    - Modified main loop to use `rustyline` for user input
    - Implemented history functionality using `rustyline`
    - Improved error handling for `rustyline` input errors
    - Added `rustyline` as a dependency in `Cargo.toml`
    - Ignored `history.txt` in `.gitignore` file

--- src/main.rs
+++ src/main.rs
@@ -2,42 +2,63 @@ mod gpt_connector;
 mod logger;
 
 use owo_colors::OwoColorize;
-use std::io;
 use gpt_connector::GPTConnector;
 use logger::Logger;
+use rustyline::Editor;
+use rustyline::error::ReadlineError;
 
-fn main() -> io::Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let gpt = GPTConnector::new();
     let logger = Logger::new();
 
     println!("Starting interactive GPT chat session. Type 'exit' to end.");
     
+    let mut rl = rustyline::DefaultEditor::new()?;
+    if rl.load_history("logs/history.txt").is_err() {
+        println!("No previous history found.");
+    }
+    
+
     loop {
-        let mut input = String::new();
-        print!("You: ");
-        use std::io::Write;
-        io::stdout().flush()?;
-        io::stdin().read_line(&mut input)?;
-        let input = input.trim();
+        let readline = rl.readline("You: ");
+        match readline {
+            Ok(line) => {
+                let input = line.trim();
 
-        if input == "exit" {
-            break;
+                if input == "exit" {
+                    rl.add_history_entry(input);
+                    rl.save_history("history.txt").unwrap();
+                    break;
+                }
+
+                let response = tokio::runtime::Builder::new_current_thread()
+                    .enable_io()
+                    .enable_time()
+                    .build()?
+                    .block_on(gpt.send_request(input))
+                    .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))?
+                    .content;
+                
+                logger.log_interaction(input, &response);
+
+                println!("GPT: {}", response.green());
+
+                rl.add_history_entry(input);
+            },
+            Err(ReadlineError::Interrupted) => {
+                println!("Interrupted");
+                break;
+            },
+            Err(ReadlineError::Eof) => {
+                println!("EOF reached");
+                break;
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
         }
-
-        let response = tokio::runtime::Builder::new_current_thread()
-            .enable_io()
-            .enable_time()
-            .build()
-            .unwrap()
-            .block_on(gpt.send_request(input))
-            .map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))?
-            .content;
-        
-        logger.log_interaction(input, &response);
-
-        println!("GPT: {}", response.green());
     }
 
     Ok(())
 }
-
