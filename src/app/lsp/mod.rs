d0e319c6bf104aaeb2e9af6ff8ebce843b664c2c
tenkai <tenkai@kariya.ai>
2024-01-26 17:03:30 -0800

    adding LSP interactiojn functioons

--- src/app/lsp/mod.rs
+++ src/app/lsp/mod.rs
@@ -1,2 +1,4 @@
 pub mod lsp_client;
+pub mod lsp_editor;
+pub mod lsp_navigation;
 pub mod lsp_stdio;
--- tests/lsp_tests.rs
+++ tests/lsp_tests.rs
@@ -1,15 +1,11 @@
 // tests/lsp_client.rs
-use lsp_types::{
-  ClientCapabilities, ClientInfo, DidOpenTextDocumentParams, GeneralClientCapabilities, InitializeParams,
-  TextDocumentClientCapabilities, TextDocumentItem, TextDocumentSyncCapability, TextDocumentSyncClientCapabilities,
-  WorkspaceFolder,
-};
-use sazid::app::lsp::lsp_client::LspClient;
-use sazid::app::lsp::lsp_stdio::LspClientStdio;
-use serde_json::json;
+use lsp_types::*;
+use ntest::timeout;
+use sazid::app::lsp::{lsp_client::LspClient, lsp_stdio::LspClientStdio};
 
 // The actual test function
 #[tokio::test]
+#[timeout(3000)]
 async fn test_rust_analyzer_connection() -> anyhow::Result<()> {
   // Create an LspClientStdio instance
   let mut lsp_client = LspClientStdio::create().await?;
@@ -20,12 +16,20 @@ async fn test_rust_analyzer_connection() -> anyhow::Result<()> {
   let s = r#"
   { "cargo": { "buildScripts": { "enable": true } }, "procMacro": { "enable": true } }
   "#;
-  // let initialization_options = Some(serde_json::from_str(s).unwrap());
+  let initialization_options = Some(serde_json::from_str(s).unwrap());
   // .split('\n')
   // .collect::<String>();
   let capabilities = ClientCapabilities {
+    workspace: Some(WorkspaceClientCapabilities {
+      workspace_folders: Some(WorkspaceFolderCapability {
+        supported: Some(true),
+        change_notifications: Some(OneOf::Left(true)),
+      }),
+      ..Default::default()
+    }),
     text_document: Some(TextDocumentClientCapabilities {
       synchronization: Some(TextDocumentSyncClientCapabilities { did_save: Some(true), ..Default::default() }),
+
       ..Default::default()
     }),
     ..Default::default()
@@ -34,28 +38,34 @@ async fn test_rust_analyzer_connection() -> anyhow::Result<()> {
     uri: url::Url::from_directory_path(test_project_path.clone()).unwrap(),
     name: "testproject".to_string(),
   }]);
+
   let root_uri = Some(url::Url::from_directory_path(test_project_path).unwrap());
   let client_info = Some(ClientInfo { name: "sazid".to_string(), version: None });
 
-  // let process_id = Some((std::process::id() as u64).try_into().unwrap());
+  let process_id = Some((std::process::id() as u64).try_into().unwrap());
   // Send `initialize` request to rust-analyzer
   let init_params = InitializeParams {
     client_info: None,
     locale: None,
     work_done_progress_params: Default::default(),
-    process_id: None,
+    process_id,
     root_path: None,
     root_uri: None,
-    initialization_options: None,
-    capabilities: ClientCapabilities::default(),
+    initialization_options,
+    capabilities,
     trace: None,
     workspace_folders: None,
   };
   // println!("init_params: {:#?}", init_params);
 
   let init_result = lsp_client.initialize(init_params).await?;
-  assert!(init_result.capabilities.text_document_sync.is_some());
+  // println!("init_result: {:#?}", init_result);
 
+  assert!(init_result.capabilities.text_document_sync.is_some());
+  // println!("initiaize_result: {:#?}", init_result);
+  let initiaized_result = lsp_client.initialized().await;
+  assert!(initiaized_result.is_ok());
+  // println!("initiaized_result: {:#?}", initiaized_result);
   // Open a dummy file
   let did_open_params = DidOpenTextDocumentParams {
     text_document: TextDocumentItem {
@@ -65,11 +75,14 @@ async fn test_rust_analyzer_connection() -> anyhow::Result<()> {
       text: "fn main() {}".into(),
     },
   };
+  // println!("did_open_params: {:#?}", did_open_params);
 
-  lsp_client.did_open(did_open_params).await?;
+  let did_open_params_result = lsp_client.did_open(did_open_params).await;
+  assert!(did_open_params_result.is_ok());
 
   // Send `shutdown` request to rust-analyzer
-  lsp_client.shutdown().await?;
+  let shutdown_result = lsp_client.shutdown().await;
+  assert!(shutdown_result.is_ok());
 
   Ok(())
 }
